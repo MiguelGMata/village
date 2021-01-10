f16a2535fdd484ab6b4687d220771dc6ee9e8f81
MiguelMata <nelsonmgarciam@gmail.com>
2021-01-10 13:46:23 +0100

    premiere git

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+const apiRouter = require('./routes/apiRouter').router;
+const helmet = require('helmet');
+const logger = require('morgan');
+const { notFoundHandler, errorLogger, errorHandler } = require('./middlewares');
+
+const app = express()
+const port = process.env.PORT || 3000
+
+//Helmet
+app.use(helmet());
+
+//Morgan
+app.use(logger('tiny'));
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//cors
+app.use(cors())
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+)
+
+//configure routes
+app.get('/', function (request, response) {
+    response.json({ message: 'salut tout le monde!' });
+});
+
+app.use('/village', apiRouter);
+
+app.use('*', notFoundHandler);
+app.use(errorLogger);
+app.use(errorHandler);
+
+app.listen(port, function () {
+    console.log('Le serveur fonctionne sur le port : ' + port)
+})
